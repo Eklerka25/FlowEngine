a066ae6ead6776c38a2197702d40b42164e318a6
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-20 15:08:31 +0100

    map.hpp update

--- include/map.hpp
+++ include/map.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <iostream>
-#include <macros.hpp>
+#include <macros.h>
 
+void overwrite_map(char value);
 void draw(int x, int y, char value);
 void dump_map();
 void draw_line(int x1, int y1, int x2, int y2, char value);
+void draw_polygon(int x1, int y1, int x2, int y2, int x3, int y3, char value);
